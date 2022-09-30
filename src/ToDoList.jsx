b8c58cb2c70343a52be72eb0351c097331b70677
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-09-30 22:32:53 +0800

    feat: navlink

---
+++ src/ToDoList.jsx
@@ -0,0 +1,9 @@
+function ToDoLint () {
+  return (
+    <div>
+      <h1>ToDoLint</h1>
+    </div>
+  )
+}
+
+export default ToDoLint
--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,66 @@
 import React from 'react'
-import { HashRouter, Link, Routes, Route } from "react-router-dom";
+import { HashRouter, NavLink, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ToDoList from './ToDoList'
 import Products from './Products'
+import Admin from './Admin'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-    <div>
-      <Link to="/">首頁</Link> |
-      <Link to={
-        { pathname: '/products', }
-      }
-        state={{
-          data: {
-            id: '1',
-            name: 'QQ 產品'
-          }
-        }}>
-        產品詳細
-      </Link>
-    </div>
+      <nav className="px-5 flex items-center h-[60px] bg-indigo-500 text-white">
+        <h1 className="mr-auto text-2xl">React TodoList</h1>
+        <ul className="flex">
+          <li className="mr-3">
+            <NavLink to="/home"
+            className={({ isActive }) => 
+              [
+                'border p-3 hover:bg-indigo-600 duration-500',
+                isActive ? 'router-link-active' : null
+              ].join(' ')
+            }
+            >Home</NavLink>
+          </li>
+          <li className="mr-3">
+            <NavLink to="/todolist"
+            activeClassName="router-link-active"
+            className={({ isActive }) => 
+              [
+                'border p-3 hover:bg-indigo-600 duration-500',
+                isActive ? 'router-link-active' : null
+              ].join(' ')
+            }
+            >ToDoList</NavLink>
+          </li>
+          <li className="mr-3">
+          <NavLink to='/products'
+            className={({ isActive }) => 
+              [
+                'border p-3 hover:bg-indigo-600 duration-500',
+                isActive ? 'router-link-active' : null
+              ].join(' ')
+            }
+            >產品詳細</NavLink>
+          </li>
+          <li className="mr-3">
+          <NavLink to='/admin'
+            className={({ isActive }) => 
+              [
+                'border p-3 hover:bg-indigo-600 duration-500',
+                isActive ? 'router-link-active' : null
+              ].join(' ')
+            }
+            >Admin</NavLink>
+          </li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={ <App /> } />
+        <Route path="/todolist" element={ <ToDoList /> } />
         <Route path="/products" element={ <Products /> } />
+        <Route path="/admin" element={ <Admin /> } />
       </Routes>
     </HashRouter>
   </React.StrictMode>
